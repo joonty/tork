8aea7a94770e9b400e7c04530ddf94d62d56ea8c
Jon Cairns <jon@ggapps.co.uk>
2014-09-26 11:26:30 +0100

    Remove unnecessary code from rspec-rails config

--- lib/tork/config/rspec-rails/config.rb
+++
@@ -1,2 +0,0 @@
-$tork_config_spec_glob = '**/{spec_*,*_spec}.rb'
-$tork_config_spec_grep = %r{.*(\bspec_[^/]+|[^/]+_spec)\.rb$}
--- lib/tork/config/rspec-rails/master.rb
+++ lib/tork/config/rspec-rails/master.rb
@@ -1,5 +1,3 @@
 $LOAD_PATH.unshift 'spec' unless $LOAD_PATH.include? 'spec'
-$LOAD_PATH.unshift 'lib' unless $LOAD_PATH.include? 'lib'
 require 'rails_helper' if File.exist? 'spec/rails_helper.rb'
 require './rails_helper' if File.exist? 'rails_helper.rb'
-require 'rspec/autorun' if defined? RSpec
